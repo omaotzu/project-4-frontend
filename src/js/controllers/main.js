07a4dddcc942e597b4661d9301922e095fa0769f
omaotzu <omarhphillips@gmail.com>
2017-04-16 11:48:06 +0100

    added in front end current user into main controller and added main controller to body - logout also working

---
+++ src/js/controllers/main.js
@@ -0,0 +1,38 @@
+angular
+  .module('myGuideBlog')
+  .controller('MainCtrl', MainCtrl);
+
+MainCtrl.$inject = ['$rootScope', '$state', '$auth'];
+function MainCtrl($rootScope, $state, $auth){
+  const vm = this;
+  vm.isAuthenticated = $auth.isAuthenticated;
+
+  $rootScope.$on('error', (e, err) => {
+    vm.stateHasChanged = false;
+    vm.message = err.data.message;
+    $state.go('login');
+  });
+
+  $rootScope.$on('$stateChangeSuccess', () => {
+    if(vm.stateHasChanged) vm.message = null;
+    if(!vm.stateHasChanged) vm.stateHasChanged = true;
+    if($auth.getPayload()) vm.currentUser = $auth.getPayload();
+  });
+
+  const protectedStates = ['someStatesWeDontHaveYet'];
+
+  $rootScope.$on('$stateChangeStart', (e, toState) => {
+    if(!$auth.isAuthenticated() && protectedStates.includes(toState.name)) {
+      e.preventDefault();
+      $state.go('login');
+      vm.message = 'You must be logged in to access this page';
+    }
+    vm.pageName = toState.name;
+  });
+
+  function logout() {
+    $auth.logout();
+    $state.go('register');
+  }
+  vm.logout = logout;
+}
